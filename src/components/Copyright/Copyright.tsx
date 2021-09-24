065e59520d7e92dbe80de17060e738658051075b
fardinkhan <fardin_2510@hotmail.com>
2021-09-24 16:30:08 -0700

    Updated Copyright.

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Typography } from "@mui/material";
 import styled from "styled-components";
 
-const Mew = styled.div`
+const CopyrightStyle = styled.div`
     margin: 5rem 0 0 0;
 `;
 
 const Copyright = () => {
     var year = new Date().getFullYear();
     return (
-        <Mew>
+        <CopyrightStyle>
             <Typography style={{color:"white"}} variant = "subtitle2">made by Fardin Khan, ©️ {year}</Typography>
-        </Mew>
+        </CopyrightStyle>
     )
 }
 
